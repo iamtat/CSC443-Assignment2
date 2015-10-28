643d748c3fd4a4cd7c84306aeee18642155cc435
Tatiana Chaer <chaertatiana@gmail.com>
2015-10-28 09:27:51 +0200

    Connection do DB

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+   $HostName= 'mysql4.000webhost.com';
+   $DB_username = 'a8830566_User';
+   $DB_password = '1a2b3c4d';
+   $DB_name = 'a8830566_Ass2'; 
+   
+   $mysqli = new mysqli($HostName, $DB_username, $DB_password, $DB_name);
+   
+
+?>
